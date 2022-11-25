389ae4e63370931380f4e1456ca7ab293d468b72
unknown <andres24968@gmail.com>
2022-11-24 21:38:04 -0600

    fix

--- src/db/database.js
+++ src/db/database.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
    await mongoose
       .connect(
-        "mongodb+srv://zus:gWuyWhVyssTZaY49@cluster0.srne526.mongodb.net/?retryWrites=true&w=majority"
+        "your string connection"
       )
       .then((s) => {
         console.log(">>>>Db is Connected");
